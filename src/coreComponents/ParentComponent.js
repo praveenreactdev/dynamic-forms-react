0d8dec3ce9751c7022657c6ec4ba176a5744ee86
Praveen <leavemeatext@gmail.com>
2019-02-13 15:31:38 +0530

    added json editor

--- src/coreComponents/ParentComponent.js
+++ src/coreComponents/ParentComponent.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import {SectionsContainer, Section} from 'react-fullpage';
 import DynamicComponent from './DynamicComponent';
 import uuid from 'uuid';
+import './styles/style.css'
+import JSONInput from 'react-json-editor-ajrm';
+import locale    from 'react-json-editor-ajrm/locale/en';
+
 const formData = [
     {
       id: uuid(),
@@ -26,21 +30,50 @@ const formData = [
           id: 'Email',
           floatingLabelText: 'Email',
         }
+      },
+      {
+        id: uuid(),
+        type: 'inputfield',
+        props: {
+          id: 'Praveen',
+          floatingLabelText: 'Praveen',
+        }
       }
   ];
 
 class ParentComponent extends Component {
+  
+  state = {
+    data: formData
+  }
 
+ onChange = (data)=>{
+   this.setState({data: data.jsObject})
+}
   render() {
     return (
       <div>
         <SectionsContainer>
             <Section>
-                <DynamicComponent formData={formData} />
+                <div id="container_editor" >
+                  <div ref="editor "id="editor">
+                  <JSONInput
+                        id          = 'editor'
+                        placeholder = { formData }
+                        locale      = { locale }
+                        onChange = {this.onChange}
+                    />
+                  </div>
+                  <div id="core_component">
+                  <DynamicComponent formData={this.state.data}/>
+                  </div>
+                </div>  
             </Section>
         </SectionsContainer>
       </div>
     )
   }
 }
+
+
 export default ParentComponent;
