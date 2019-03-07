08b1ee97d74ed2a57e95fb7b85e90e49d66268c7
praveenatsolartis <leavemeatext@gmail.com>
2019-03-07 23:01:40 +0530

    added CustomComponentV2 with select

--- src/coreComponents/v2Components/CustomComponentV2.js
+++ src/coreComponents/v2Components/CustomComponentV2.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
-
+import 'materialize-css/dist/css/materialize.min.css';
+import M from "materialize-css";
 class CustomComponentV2 extends Component {
+
+  componentDidMount() {
+    M.AutoInit();
+}
+
+
   render() {
     const component = this.props.data;
     console.log(component)
+    const options = [{value:1,data:'Option 1'},{value:2,data:'Option 2'}]
+    if(component.ComponentType === 'Select')
     return (
-      <div className="ui container">
+     
       <div className="input-field">
-              <label htmlFor={component.ComponentId}>{component.ComponentLabel}</label>
-                <input type="text" id={component.ComponentId} style={{color:'black'}} ref={component.ComponentId} 
-                placeholder={component.PlaceHolderText} onChange={e=>console.log(e.target.value)}
-             />
-      </div>
+          <select>
+            <option value="" disabled selected>Choose your option</option>
+           {
+             options.map(option=>{
+               return <option value={option.value} key={option.value}>{option.data}</option>
+             })
+           }
+        </select>
+      <label>Materialize Select</label>
       </div>
     )
+    else{
+      return <div/>
+    }
   }
 }
 export default CustomComponentV2
